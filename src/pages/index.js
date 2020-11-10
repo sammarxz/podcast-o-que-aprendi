8fdc3d3ad6cc531bf6272ad9b46205c2338d4db1
Sam Marxz <sammarxzz@gmail.com>
2020-11-10 08:47:24 -0300

    add eslint jsx rule

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Home = () => <div>Hello world!</div>;
+
+export default Home;
